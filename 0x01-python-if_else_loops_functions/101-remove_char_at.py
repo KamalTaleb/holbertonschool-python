b8e0f245e49c536aeb8ae6bfd05c3c91a61394e6
kamal taleb <kamal.talebb@gmail.com>
2020-09-02 20:56:26 +0300

    remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    strtm = ""
+    cont = 0
+    for c in str:
+        if cont == n:
+            pass
+        else:
+            strtm += c
+        cont += 1
+    return strtmp
