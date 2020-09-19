49a2a6a18f28794de80d96be2727c00d713e106c
kamal taleb <kamal.talebb@gmail.com>
2020-09-19 11:15:14 +0300

    integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-
-"""
-function 0-add_integer
-adds two integers
-module "0-add_integer"
-"""
+"""kamal"""
 
 
 def add_integer(a, b=98):
-    """adds two numbers
-    >>> add_integer(4, 3)
-    7
-    >>> add_integer(5, 5)
-    10
-    """
+    """kamal"""
     fnum = 0
     if type(a) is int or type(a) is float:
         fnum += int(a)
