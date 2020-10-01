a898a1b669fbfd86c8c6393d70bb356d044cba69
kamal taleb <kamal.talebb@gmail.com>
2020-10-01 12:57:20 +0300

    number of lines

---
+++ 0x0C-python-input_output/1-number_of_lines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""print number of lines"""
+
+
+def number_of_lines(filename=""):
+    """
+    print number lines
+    """
+    line_number = 0
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            line_number += 1
+    return line_number
