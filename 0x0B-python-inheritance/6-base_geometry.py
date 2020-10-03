6f0ef612a126bd9b11d6d1cb960d6811a22fa826
kamal taleb <kamal.talebb@gmail.com>
2020-10-03 20:18:14 +0300

    Improve Geometry

---
+++ 0x0B-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """
+    Improve Geometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
