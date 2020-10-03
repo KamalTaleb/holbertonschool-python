658eb2b7572fe24ffb556c3edb4da7ea7de1039c
kamal taleb <kamal.talebb@gmail.com>
2020-10-03 20:11:03 +0300

    exact same object

---
+++ 0x0B-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""is the same class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    is the same
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
