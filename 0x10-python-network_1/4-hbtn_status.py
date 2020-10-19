04b3f6014c532bd81757fdf2cd9cf31043ae2218
kamal <kamal.talebb@gmail.com>
2020-10-19 22:14:25 +0300

    hbtn status

---
+++ 0x10-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" hbtn status"""
+
+
+import requests
+
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}\n\t- content: {}".format(
+        type(r.text), r.text))
