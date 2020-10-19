862dfb2b78ed5a7a0ee6d9e26ce1ff703185d94e
kamal <kamal.talebb@gmail.com>
2020-10-19 21:31:11 +0300

    hbtn status

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-""" hbtn status"""
+"""hbtn status"""
 
 
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html.decode('utf-8')))
+from urllib import (request)
+
+if __name__ == "__main__":
+    req = request.Request("https://intranet.hbtn.io/status")
+    with request.urlopen(req) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf8')))
