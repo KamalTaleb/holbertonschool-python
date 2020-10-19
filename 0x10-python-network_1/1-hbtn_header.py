94caf2c8e24977b54a285eedbc58a305aba81d39
kamal <kamal.talebb@gmail.com>
2020-10-19 21:44:45 +0300

    hbtn header

---
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" hbtn header"""
+
+
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.info().get('X-Request-Id'))
