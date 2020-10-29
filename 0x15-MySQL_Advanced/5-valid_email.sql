324d951719ed621e44d73602689697977750ea70
kamal <kamal.talebb@gmail.com>
2020-10-29 19:52:49 +0200

    valid email

---
+++ 0x15-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- valid_email
+DELIMITER |
+CREATE TRIGGER email_bool BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END;
+|
