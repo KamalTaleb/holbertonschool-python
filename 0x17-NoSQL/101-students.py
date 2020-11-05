70e0141a383b82d266fff90b216557bdc6924220
kamal <kamal.talebb@gmail.com>
2020-11-05 18:45:09 +0200

    top students

---
+++ 0x17-NoSQL/101-students.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""students"""
+
+def top_students(mongo_collection):
+    """sort top students"""
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
